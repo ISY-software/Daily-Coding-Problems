8caf54dffe13a65a2bdf8bcbb165aa664cbc5739
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2022-09-20 16:33:20 -0500

    Fix the remplace behaviour of CMP

--- dcp11/solution.py
+++ dcp11/solution.py
@@ -10,21 +10,22 @@ class Node():
 
 
 def printTrzee(tree):
-    lst = []
-    phelper(tree, lst, 0)
-    print(lst)
+    lst, mxln = lhelper(tree)
+#   phelper(lst, mxln)
+    phelper2(lst, mxln)
 
 
-def phelper(tree, lst, lvl):
+def lhelper(tree):
     lvl = 0
     rootPath = [tree]
-    rsltL = [[]]
+    rsltL = []
     loopT = 1
-    count = 0
     expand = True
+    maxStrLen = 4
     while expand:
         expand = False
-        for i in range(loopT):
+        rsltL.append([])
+        for _ in range(loopT):
             tmpTree = rootPath.pop(0)
             if tmpTree is None:
                 rootPath.append(None)
@@ -33,20 +34,50 @@ def phelper(tree, lst, lvl):
                 continue
             expand = True
             rsltL[lvl].append(tmpTree.val)
+            if maxStrLen < len(str(tmpTree.val)):
+                maxStrLen = len(str(tmpTree.val))
             rootPath.append(tmpTree.left)
             rootPath.append(tmpTree.right)
-        lvl+=1
-        rsltL.append([])
-        loopT *=2
+        lvl += 1
+        loopT *= 2
 
-    print(rsltL)
+    return rsltL, maxStrLen
+
+
+def phelper(lst, mxln):
+    print(mxln)
+    initSpaces = 2 ** (len(lst)-2)
+    betweenSpaces = (initSpaces*2) - 1
+    for i in range(len(lst)-1):
+        elem = lst[i]
+        print(" "*initSpaces, end="")
+        for i in range(len(elem)):
+            if elem[i] is None:
+                print("#", end='')
+            else:
+                print(elem[i], end='')
+            print(" "*(betweenSpaces), end='')
+        print()
+        initSpaces = int(initSpaces/2)
+        betweenSpaces = (initSpaces*2) - 1
+
+
+def phelper2(lst, mxln):
+    lenLst = len(lst)-1
+    # beetwrspaces = 1
+    for i in range(lenLst):
+        initSpaces = (2**(lenLst-i)) - 1
+        betweenSpaces = (initSpaces * 2) + 1
+        print(initSpaces, betweenSpaces)
+    pass
+
 
 treee = Node('a',
              Node('b',
                   Node('C'),
                   Node('d',
-                       Node ('I'),
-                       Node ('J'))
+                       Node('I'),
+                       Node('J'))
                   ),
              Node('e',
                   Node('f',
@@ -54,7 +85,8 @@ treee = Node('a',
                        ),
                   Node('g',
                        None,
-                       Node ('K'))
+                       Node('K'))
                   )
              )
+
 printTrzee(treee)
